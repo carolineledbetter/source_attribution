9a29e1ad86ced1c94c9a3ef3b80cda7f8af52663
Caroline Ledbetter <caroline.ledbetter@ucdenver.edu>
2018-11-16 14:17:41 -0700

    formatting, tidying, hovering instructions, months, hospital cases, etc.

---
+++ SourceAttribution/ReadMe.md
@@ -0,0 +1,17 @@
+# Source Attribution
+**PI:** Alice White  
+**Analysts:** David Weitzencamp, PhD Caroline Ledbetter, MPH  
+**Project Start Date:** May 16, 2018  
+
+### About this project:
+Epidemiologist investigating foodborne disease outbreaks often use 
+characteristics of the outbreak to narrow down the food source. For example, 
+outbreaks with a higher percentage of women are more likely to be from salad 
+than beef. Outbreaks with a small geographic reach are more likely to be dairy, 
+etc. Epidemiologists incorporate experience and knowledge to help guide their 
+search. This project seeks to help formalize some of that guidance by developing
+a predictive model. This project uses data obtained from the [CDC's National
+Outbreak Reporting System (NORS)](https://www.cdc.gov/nors/about.html) from 
+1998 to 2016. This app should only be used as a tool to guide outbreak 
+investigations and should not be considered definitive on it's own. 
+In addition, only prediction of the most common sources is possible. 
--- SourceAttribution/app.R
+++ SourceAttribution/app.R
@@ -1,3 +1,9 @@
+###########################################
+# Project: P1330White
+# Author: David Weitzenkamp & Caroline Ledbetter
+# Date: 11/16/2018
+# #########################################
+
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
@@ -7,53 +13,130 @@
 #    http://shiny.rstudio.com/
 #
 library(shiny)
-library(kknn)
-library(tidyr)
-library(ggplot2)
+library(kknn) # necessary to predict with algorithm we chose
+library(tidyr) # for gather
+library(ggplot2) # for plotting
+
+# load prediction algortithm
 load(file = 'knnnmodelobj.rda')
+
+# load empty data frame with required predictors and possible levels
 load(file = 'FileSkeleton.rda')
+
+# classify rare serotypes according to primary source
 NonSpecific <- c('Agona', 'Anatum','Berta', 'Mbandaka', 'Muenchen', 'Saintpaul', 
                  'Stanley', 'Thompson')
 PrimaryAnimal <- c('Derby', 'Group B', 'Hadar', 'Infantis', 'Johannesburg', 
                    'Montevideo', 'Oranienburg', 'Reading', 'Sandiego', 
                    'Typhimurium var Cope', 'Uganda')
 PrimaryPlant <- c('Cubana', 'Poona', 'Senftenberg', 'Virchow')
+
+# format salmonella serotypes for tidy appearance in webplatform
 SalmSeroTypes <- levels(input_skeleton$Agent)
 SalmSeroTypes <- SalmSeroTypes[!SalmSeroTypes %in% c('NonSpecific Sero group', 
                                     "Primary Animal Sero group", 
                                     "Primary Plant Sero group")]
 Serogroups <- c(SalmSeroTypes, NonSpecific, PrimaryAnimal, 
                 PrimaryPlant)
+Serogroups <- Serogroups[order(Serogroups)]
 names(Serogroups) <- Serogroups
 names(Serogroups)[Serogroups %in% 'Salm unk sero'] <- 'Unknown'
 names(Serogroups)[Serogroups %in% 'Rare'] <- "Other"
 names(Serogroups)[Serogroups %in% 'STEC'] <- " "
+
 Serogroups <- as.list(Serogroups)
-# Define UI for application that draws a histogram
+
+# format Geography for tidy appearance in webplatform
+Geography <- as.list(levels(input_skeleton$Geography))
+names(Geography) <- Geography
+names(Geography)[Geography == 'Missing'] <- 'Unknown'
+
+# Set up named month list
+Months <- as.list(1:12)
+names(Months) <- month.name
+
+# Define User Interface
 ui <- fluidPage(
-
+    # make required flags red
+    tags$style(type = "text/css", 
+               ".required{color: red;}"), 
+    # format title
+    tags$style(type = 'text/css', 
+               ".title{color: #006600; 
+                       text-align:center;
+                       }"),
+    # format logo image
+    tags$style(type = 'text/css', 
+               ".logo{position: fixed; 
+                      right: 5%;
+                      bottom: 5%; 
+                      width: 10%;
+                      }"), 
+    # format instuctions hover
+    tags$style(type = 'text/css', 
+               'span.inst:hover + div {display: block;
+                            }'), 
+    tags$style(type = 'text/css', 
+               'div.inst{display: none;
+                         color: #ff8000;
+                            }'), 
     # Application title
-    titlePanel("Source Prediction"),
+    div(class = 'title', 
+        titlePanel("Foodbourne Outbreaks: Source Attribution Prediction")),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar with inputs for model and instructions
     sidebarLayout(
         sidebarPanel(splitLayout(
+            # Left side of sidebar
             wellPanel(
-                numericInput("TotalCases",
-                             "Total Cases:",
+                    numericInput("TotalCases",
+                             label = span(
+                                 span(class = 'inst', 'Total Cases', 
+                                      div(class = 'required', 
+                                          '*required')),  
+                                 div(class = 'inst', 
+                                     'All suspected and confirmed cases') 
+                                 ),
                              min = 1,
                              max = Inf, 
-                             value = 0), 
-                selectInput('Season', 'Season', 
+                             value = 0),
+                    
+                selectInput('Month', 
+                            span("Month of first illness onset", 
+                                 div(class = 'required', 
+                                     '*required')), 
                             choices = c('Choose one' = '', 
-                                        levels(input_skeleton$Season))), 
-                radioButtons('Geography', 'Geography', 
-                             choices = levels(input_skeleton$Geography)), 
-                radioButtons('.bacteria', 'Infectious Agent', 
+                                        Months)), 
+                radioButtons('Geography', 
+                             label = span(
+                                 span(class = 'inst', 
+                                      "Geography of exposures", 
+                                      div(class = 'required', 
+                                          '*required')),  
+                                 div(class = 'inst', 
+                                     'The geography of the suspected exposures,',  
+                                     br(), 
+                                     'not the residence of the cases.')), 
+                             choices = Geography, 
+                             selected = character(0)),
+                radioButtons('.bacteria', 
+                             span('Infectious Agent', 
+                                  div(class = 'required', 
+                                      '*required')), 
                              selected =  character(0), 
                              choiceNames = c('STEC', 'Salmonella'), 
                              choiceValues = 1:2), 
-                selectInput('Agent', 'Salmonella Serotype', 
+                selectInput('Agent', 
+                            label = span(
+                                span(class = 'inst', 
+                                     'Salmonella Serotype', 
+                                     div(class = 'required', 
+                                         '*required for Salmonella')),  
+                                div(class = 'inst', 
+                                    'If the suspected agent is Salmonella,', 
+                                    br(), '
+                                    you must select a serotype. Other, ', br(), 
+                                    'Unknown, and Multiple are options.')),
                             choices = c('Choose STEC or Salmonella' = "")), 
                 sliderInput('GenderMale', 'Male', 
                             min = 0, 
@@ -70,14 +153,14 @@ ui <- fluidPage(
                             max = 0, 
                             value = 0, 
                             ticks = F, step = 1),
-                sliderInput('HospPercent2', 'Percent Hospitalized', 
+                sliderInput('Hosp', 'Cases Hospitalized', 
                             min = 0, 
-                            max = 100, 
-                            post = '%', 
+                            max = 0, 
                             ticks = F, 
-                            value = 0)
+                            value = 0, step = 1)
+                , style = "width:350px;"
                 ), 
-            
+            # Right side of Sidebar Age inputs and submit button
             wellPanel(
                 numericInput('AgeUnder1', 'Under 1 Year', 
                              min = 0, 
@@ -107,47 +190,73 @@ ui <- fluidPage(
                              min = 0, 
                              max = 0, 
                              value = 0),
-                numericInput('AgeUnknown', 'AgeUnknown', 
+                numericInput('AgeUnknown', 'Age Unknown', 
                              min = 0, 
                              max = 0, 
                              value = 0), 
                 actionButton('.Submit', 'Submit')
-                )
-            )
+                , style = "width:150px;"
+                ), cellWidths = c('350px', '150px')
             ), 
-    
-        mainPanel(plotOutput("Results"))
+            # instuction paragraph at the bottom of the sidebar
+            # spans both models
+            p(paste0('Instructions: This is a tool designed to help guide an ', 
+                     'investigation of a foodbourne outbreak caused by ', 
+                     'STEC or Salmonella. This is only ', 
+                     'meant to be a guide and can only predict some of the ', 
+                     'most common food sources. The more information you can ', 
+                     'provide the better the prediction will preform. ', 
+                     '**Additional instructions to be provided by Alice. **')), 
+            style = 'width:550px;'
+                     
+            ), 
+        
+        # output results
+        div(mainPanel(
+            div(plotOutput("Results"), style = 'align:right'), 
+            img(src = 'CoE logo no bckgrnd.png', class = 'logo')), 
+            style = 'margin-left:550px')
     )
 )
 
 
-# Define server logic required to draw a histogram
+# Define server logic required to process inputs and produce results
 server <- function(input, output, session) {
-
+    
+    # run model only once submit is pressed
     observeEvent(input$.Submit, {
         output$Results <- renderPlot({
+            # isolate so that after submit is pressed, model is not rerun
+            # until it is pressed again (prevent dynamic updating)
             isolate({
-                total <- sum(input$AgeUnder1, 
-                             input$Age1to4, 
-                             input$Age5to9, 
-                             input$Age10to19, 
-                             input$Age20to49, 
-                             input$Age50to74, 
+                total <- sum(input$AgeUnder1,
+                             input$Age1to4,
+                             input$Age5to9,
+                             input$Age10to19,
+                             input$Age20to49,
+                             input$Age50to74,
                              input$Age75plus)
+                # confirm all required fields are valid
                 validate(
                     need(input$TotalCases > 0,
                          'You must have at least one case'),
-                    need(input$Season != "",
-                         'You must choose a season'),
+                    need(length(input$Geography) > 0, 
+                         'You must select a geography of expoure'), 
+                    need(input$Month != "",
+                         'You must choose a month'),
                     need(input$Agent != "",
                          paste0('You must choose an an infectious agent ',
                                 'and a salmonella serotype if the agent is ',
                                 'salmonella')),
-                    need(total <= input$TotalCases, 
-                         paste0('The sum of number of cases in each age group ', 
+                    need(total <= input$TotalCases,
+                         paste0('The sum of number of cases in each age group ',
                                 'can not exceed the total number of cases'))
                 )
+                # process inputs into a dataframe
                 Inputs <- reactiveValuesToList(input)
+                
+                # convert misc salmonella serotypes to a format recognizable 
+                # to the model
                 Agent <- Inputs$Agent
                 if(Agent %in% NonSpecific) Agent <- 'NonSpecific Sero group'
                 if(Agent %in% PrimaryAnimal) Agent <-
@@ -155,81 +264,67 @@ server <- function(input, output, session) {
                 if(Agent %in% PrimaryPlant) Agent <- "Primary Plant Sero group"
                 Inputs$Agent <- Agent
                 Inputs <- data.frame(Inputs)
-                Inputs <- subset(Inputs,
-                                 select = -c(`Season.selectized`,
-                                             `Agent.selectized`))
+                Inputs$Season <- cut(as.numeric(Inputs$Month),
+                                     breaks = c(seq(1, 12, 3), 12),
+                                     include.lowest = T, right = F,
+                                     labels = c('Winter', 'Spring', 'Summer',
+                                                'Fall'))
+                # coverts raw numbers to percentages
                 SexVars <- grep('^Gender', names(Inputs), value = T)
                 AgeVars <- grep('^Age[^n]', names(Inputs), value = T)
                 PercentSex <- paste0('Percent', SexVars)
                 PercentSex <- gsub('Gender', "", PercentSex)
                 PercentAge <- paste0('Percent', AgeVars)
-                Inputs[, PercentSex] <- lapply(Inputs[, SexVars], 
+                Inputs[, PercentSex] <- lapply(Inputs[, SexVars],
                                                function(x){
                                                    y <- x/Inputs$TotalCases*100
                                                    return(y)
                                                })
-                Inputs[, PercentAge] <- lapply(Inputs[, AgeVars], 
+                Inputs[, PercentAge] <- lapply(Inputs[, AgeVars],
                                                function(x){
                                                    y <- x/Inputs$TotalCases*100
                                                    return(y)
                                                })
+                Inputs$HospPercent2 <- Inputs$Hosp/Inputs$TotalCases*100
+                
+                # keep only predictors in the model
                 Inputs <- subset(Inputs,
                                  select = names(input_skeleton))
+                
+                # paste predictors into the empty data frame so levels/structure
+                # is preserved
                 input_skeleton[1, ] <- Inputs[1, ]
                 pred <- predict(finalchoice, newdata = input_skeleton,
                                 type = 'prob')
+                # convert probabilites from 4 columns to 2 (one with source,
+                # one with probality) for ggplot
                 pred <- gather(pred, 1:4, key = 'source', value = 'prob')
+                
+                # plot
                 ggplot(data = pred, aes(x = source, y = prob)) +
                     geom_col(aes(fill = source)) +
                     theme_classic() +
                     labs(x = 'Source',
                          y = 'Predicted Probability',
                          title = 'Predicted Probability of Potential Sources') +
-                    guides(fill = 'none') + 
+                    guides(fill = 'none') +
                     geom_label(aes(label = round(prob, 2)))
             })
         }, width = 800, height = 800, res = 200)
-        # output$test <- renderPrint({
-        #     Inputs <- reactiveValuesToList(input)
-        #     Agent <- Inputs$Agent
-        #     if(Agent %in% NonSpecific) Agent <- 'NonSpecific Sero group'
-        #     if(Agent %in% PrimaryAnimal) Agent <- 
-        #         "Primary Animal Sero group"
-        #     if(Agent %in% PrimaryPlant) Agent <- "Primary Plant Sero group"
-        #     Inputs$Agent <- Agent
-        #     Inputs <- data.frame(Inputs)
-        #     Inputs <- subset(Inputs,
-        #                      select = -c(`Season.selectized`,
-        #                                  `Agent.selectized`))
-        #     SexVars <- grep('^Gender', names(Inputs), value = T)
-        #     AgeVars <- grep('^Age[^n]', names(Inputs), value = T)
-        #     PercentSex <- paste0('Percent', SexVars)
-        #     PercentSex <- gsub('Gender', "", PercentSex)
-        #     PercentAge <- paste0('Percent', AgeVars)
-        #     Inputs[, PercentSex] <- lapply(Inputs[, SexVars], 
-        #                                    function(x){
-        #                                        y <- x/Inputs$TotalCases*100
-        #                                        return(y)
-        #                                    })
-        #     Inputs[, PercentAge] <- lapply(Inputs[, AgeVars], 
-        #                                    function(x){
-        #                                        y <- x/Inputs$TotalCases*100
-        #                                        return(y)
-        #                                    })
-        #     Inputs <- subset(Inputs,
-        #                      select = names(input_skeleton))
-        #     str(Inputs)
-        #     
-        # })
     })
     
-
+    # update salmonella serotype dropdown based on STEC/salmonella selection
     observeEvent(input$.bacteria, {
-        if(input$.bacteria == 1) choice <- Serogroups[9]
-        if(input$.bacteria == 2) choice <- c(list('Choose one' = ""), 
-                                            Serogroups[-9])
+        if(input$.bacteria == 1) choice <- list(" " = 'STEC' )
+        if(input$.bacteria == 2) {
+            excl <- which(names(Serogroups) == ' ')
+            choice <- c(list('Choose one' = ""), 
+                                            Serogroups[-excl])
+            }
         updateSelectInput(session, 'Agent', choices = choice)
     })
+    
+    # update possibe gender, age amd hospilization cases based on total cases
     observeEvent(input$TotalCases, {
         tot <- input$TotalCases
         updateSliderInput(session, 'GenderMale', 
@@ -252,6 +347,10 @@ server <- function(input, output, session) {
         updateNumericInput(session, 'AgeUnknown', 
                            max = input$TotalCases - total, 
                            value = input$TotalCases - total)
+        updateSliderInput(session, 'Hosp', 
+                          max = input$TotalCases)
+        ### Max doesn't really need to be updated as it doesn't prevent you
+        ### from enteriing a higher value
         # updateNumericInput(session, 'AgeUnder1', 
         #                    max = input$TotalCases)
         # updateNumericInput(session, 'Age1to4', 
@@ -268,6 +367,7 @@ server <- function(input, output, session) {
         #                    max = input$TotalCases)
     })
     
+    # update Female and Unknown Gender cases based on Male/Female cases
     observeEvent(input$GenderMale, {
         updateSliderInput(session, 'GenderFemale', 
                           min = 0, 
@@ -281,6 +381,8 @@ server <- function(input, output, session) {
                           max = val, 
                           val = val)
     })
+    
+    # update unknown age to be difference of total cases and all age groups
     observeEvent(input$AgeUnder1|input$Age1to4|
                      input$Age5to9|input$Age10to19|
                      input$Age20to49|input$Age50to74|
