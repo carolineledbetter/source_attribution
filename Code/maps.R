cce9308389e44b196e5eab9c33d3eacd2b2d318a
Caroline Ledbetter <caroline.ledbetter@ucdenver.edu>
2019-11-18 15:26:44 -0700

    fixed incidence equation and District of Columbia

--- Code/maps.R
+++ Code/maps.R
@@ -20,7 +20,7 @@ post_2010_pop <- read_csv("DataRaw/nst-est2018-alldata.csv",
                                                 POPESTIMATE2018 = col_double()
                                                 )
                           )
-
+us_map <- fiftystater::fifty_states
 pre_2010_pop %>% 
   rename(NAME = `...1`) %>% 
   rename_at(vars(-NAME), ~paste0('POPESTIMATE', .)) %>% 
@@ -31,7 +31,11 @@ pre_2010_pop %>%
   rename(state = NAME) -> population_data
 
 NORSData %>% janitor::clean_names() %>% 
-  filter(primary_mode == 'Food') %>% 
+  filter(primary_mode == 'Food') %>%
+  mutate(state = if_else(state == "Washington DC", 
+                         "District of Columbia", 
+                         state)
+         ) %>% 
   left_join(population_data) %>% 
   group_by(state) %>% 
   mutate(min = min(illnesses, na.rm = T), 
@@ -42,20 +46,24 @@ NORSData %>% janitor::clean_names() %>%
          total_outbreaks = n()) %>% 
   group_by(state, min, mean, median) %>% 
   fill(population, .direction = 'up') %>% 
-  summarise(illness_per_100000 = sum(total_illness)/sum(population)*100000, 
-            outbreaks_per_100000 = sum(total_outbreaks/sum(population)*100000)
-            ) -> outbreak_data
+  summarise(illness_per_100000 = sum(total_illness)/sum(population)/
+              n_distinct(year)*100000, 
+            outbreaks_per_100000 = sum(total_outbreaks)/sum(population)/
+                                         n_distinct(year)*100000
+            ) %>% 
+  ungroup() -> outbreak_data
 make_map <- function(fill_var, legend) {
   fill_var <- enquo(fill_var)
   ggplot(outbreak_data, aes(fill = !!fill_var)) + 
-    geom_map(aes(map_id = str_to_lower(state)), map = maps) + 
-    expand_limits(x = maps$long, y = maps$lat) + 
+    geom_map(aes(map_id = str_to_lower(state)), map = us_map) + 
+    expand_limits(x = us_map$long, y = us_map$lat) + 
     theme(panel.grid = element_blank(), 
           panel.background = element_blank(), 
           axis.text = element_blank(), 
           axis.ticks = element_blank(), 
           axis.line = element_blank(), 
-          axis.title = element_blank()) + 
+          axis.title = element_blank(), 
+          legend.position = c(0.9,0.2)) + 
     labs(fill = legend, 
          title = 'Foodborne Outbreaks: 1998-2017', 
          subtitle = 'All Etiologies') -> map_plot
@@ -64,8 +72,8 @@ make_map <- function(fill_var, legend) {
 
 #+ echo = FALSE, fig.width = 9
 
-make_map(illness_per_100000, 'Illness Incidence per 100,000 people')
-make_map(outbreaks_per_100000, 'Outbreak Incidence per 100,000 people')
+make_map(illness_per_100000, 'Illness Incidence per \n 100,000 people')
+make_map(outbreaks_per_100000, 'Outbreak Incidence per \n 100,000 people')
 make_map(min, 'Minimun Outbreak Size')
 make_map(mean, 'Mean Outbreak Size') 
 make_map(median, 'Median Outbreak Size')
